136d88951d8008dc79294d92bd70849d0f367a9d
abbykleespie <69874928+abbykleespie@users.noreply.github.com>
2021-03-12 13:36:16 -0600

    first program

--- Assignment3.appstudio/forms/cityWhile/cityWhile.js
+++ Assignment3.appstudio/forms/cityWhile/cityWhile.js
@@ -1,9 +1,22 @@
+
 let cities = []
+let doneWithCities = false
 let ask = ""
-console.log(cities.length)
-while (ask != "stop")
-  ask = prompt("Name a city or say stop")
-  cities.push(ask)
-  
-//while (cities.length != 0))
- // console.log(cities[i])
+let i = 0
+
+
+while (doneWithCities == false) {
+  ask = prompt("Do you have more cities? Yes or No")
+  if (ask == "Yes") {
+    doneWithCities = false
+    newCity = prompt("Enter new city")
+    cities.push(newCity)
+  } else {
+    doneWithCities = true
+  }
+}
+
+while (i < cities.length) {
+  console.log(cities[i].toLowerCase())
+  i+= 1
+}
